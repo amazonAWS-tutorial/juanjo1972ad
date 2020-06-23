08cf8b7cac54cb924ebec62c6f78f2720522aab9
amazonAWS-tutorial <67293996+amazonAWS-tutorial@users.noreply.github.com>
2020-06-23 11:22:27 +0200

    first commit

---
+++ amazon-ecs-nodejs-microservices-juanjo1972ad/3-microservices/services/posts/server.js
@@ -0,0 +1,40 @@
+const app = require('koa')();
+const router = require('koa-router')();
+const db = require('./db.json');
+
+// Log requests
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+router.get('/juanjo1972ad/posts-juanjo1972ad/in-thread/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.posts.filter((post) => post.thread == id);
+});
+
+router.get('/juanjo1972ad/posts-juanjo1972ad', function *() {
+  this.body = db.posts;
+});
+
+router.get('/juanjo1972ad/posts-juanjo1972ad/by-user/:userId', function *() {
+  const id = parseInt(this.params.userId);
+  this.body = db.posts.filter((post) => post.user == id);
+});
+
+router.get('/juanjo1972ad/', function *() {
+  this.body = "API ready to receive requests";
+});
+
+router.get('/', function *() {
+  this.body = "Ready to receive requests";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
+
+console.log('Worker started');
